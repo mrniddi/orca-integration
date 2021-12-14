21922d70841b6a83eb669af515d79e635dce5a51
Chris <invention.factory@web.de>
2021-12-14 18:43:41 +0100

    replaced "__replace_me__" with "orca"

--- packages/theme/nuxt.config.js
+++ packages/theme/nuxt.config.js
@@ -50,11 +50,11 @@ export default {
       // @core-development-only-end
       useRawSource: {
         dev: [
-          '@vue-storefront/__replace_me__',
+          '@vue-storefront/orca',
           '@vue-storefront/core'
         ],
         prod: [
-          '@vue-storefront/__replace_me__',
+          '@vue-storefront/orca',
           '@vue-storefront/core'
         ]
       }
@@ -63,8 +63,8 @@ export default {
     ['@vue-storefront/nuxt-theme', {
       generate: {
         replace: {
-          apiClient: '@vue-storefront/__replace_me__-api',
-          composables: '@vue-storefront/__replace_me__'
+          apiClient: '@vue-storefront/orca-api',
+          composables: '@vue-storefront/orca'
         }
       }
     }],
@@ -72,7 +72,7 @@ export default {
     /* project-only-start
     ['@vue-storefront/nuxt-theme'],
     project-only-end */
-    ['@vue-storefront/__replace_me__/nuxt', {}]
+    ['@vue-storefront/orca/nuxt', {}]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
